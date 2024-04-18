d893385f7d0234002be51204f435b82b68134027
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2024-04-18 16:46:44 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ themeSwitchInput.addEventListener("click", function () {
     if (body.classList.contains("light")) {
         body.classList.remove("light");
         ball.classList.remove("move-right");
-        h1.innerText = "Stealthy Quincy";
+        h1.innerText = "Stealth Quincy";
     } else {
         body.classList.add("light");
         ball.classList.add("move-right");
