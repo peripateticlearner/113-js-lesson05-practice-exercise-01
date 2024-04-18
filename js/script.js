59092a8b0890562ee4610e54f9647df0ae6ac583
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2024-04-18 16:14:51 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+var themeSwitchInput = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
 
 fans.addEventListener("mouseover", function() {
     fans.innerText = "123K";
